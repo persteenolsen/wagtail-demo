62f6f3bff75cacb8b1a59e65442d6988ff405d81
Per Olsen <persteen22@gmail.com>
2025-10-28 13:13:48 +0100

    Adjusted url for media

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -21,13 +21,13 @@ urlpatterns = [
 
 # Developement only
 # Debug=True in settings for this to work
-if settings.DEBUG:
-   from django.conf.urls.static import static
-   from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#if settings.DEBUG:
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
    # Serve static and media files from development server
-   urlpatterns += staticfiles_urlpatterns()
-   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 urlpatterns = urlpatterns + [
